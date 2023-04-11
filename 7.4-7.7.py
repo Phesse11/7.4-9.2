bbbcfcfe0584823919941f20a234682dd7884508
Paa-Nii <paaniikhesse@gmail.com>
2023-04-10 21:21:41 -0400

    Files
    The files because it didnt save for some reason

---
+++ 7.4-7.7.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+things = ['Mozzarella', 'cinderella', 'salmonella'] # The programming  list 
+things[0:3] # How many times it prints the list 
+
+
+# In[3]:
+
+
+things = ['Mozzarella', 'cinderella', 'salmonella'] # The programming lists 
+for i in things:
+    print(i.capitalize())
+things # No Changes 
+
+
+# In[5]:
+
+
+things = ['Cheesy'] # the element of things 
+for i in things: # 
+    print(i.capitalize()) # No changes in the program 
+
+
+# In[10]:
+
+
+things = ['Mozzarella', 'cinderella', 'salmonella'] 
+things  # The list of things 
+['Mozzarella', 'cinderella', 'salmonella']
+things.clear() # clears the list 
+things
+things = ['Congrats You Won The Nobel Prize'] # i change the list to Congrats you won the nobel prise 
+for i in things:
+    print(things)
+
+
+# In[ ]:
+
+
+
+
